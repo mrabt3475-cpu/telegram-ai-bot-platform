a4f094e59128d7d4c28afb693bb1ad474cec8095
mrabt3475-cpu <mrabt3475@gmail.com>
2026-05-10 11:51:31 +0100

    feat: add next config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com'],
+  },
   async rewrites() {
     return [
-      { source: '/api/:path*', destination: 'http://localhost:3000/api/:path*' }
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:3000/api/:path*',
+      },
     ];
-  }
+  },
 };
+
 module.exports = nextConfig;
